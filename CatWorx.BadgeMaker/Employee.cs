433fad2d3f1a12ebaf00252948fb60cdae647552
jesse sheehan <jessetsheehan@gmail.com>
2022-10-10 10:13:14 -0400

    finished Employee class, Program prints out full name, id, and photoUrl

---
+++ CatWorx.BadgeMaker/Employee.cs
@@ -0,0 +1,35 @@
+namespace CatWorx.BadgeMaker{
+
+    // reminder, variable declarations look like: dataType variableName = value 
+
+    class Employee
+        {
+            //write string? with a ? to say "null is ok" - make the variable "nullable"
+        private string FirstName;
+        private string LastName;
+        private int Id;
+        private string PhotoUrl;
+
+        public Employee(string firstName, string lastName, int id, string photoUrl) {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            PhotoUrl = photoUrl;
+        }
+
+        public string GetFullName() {
+            return FirstName + " " + LastName;
+        }
+
+        public int GetId() {
+            return Id;
+        }
+
+        public string GetPhotoUrl() {
+            return PhotoUrl;
+        }
+
+
+        }
+
+}
--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -8,42 +8,60 @@ namespace CatWorx.BadgeMaker
 {
     class Program 
     {
-        static List<string>  GetEmployees() {
+        static List<Employee> GetEmployees() {
             //return a list of strings:
-            List<string> employees = new List<string>(); // {"adam", "amy"};
+            List<Employee> employees = new List<Employee>(); // {"adam", "amy"};
             // employees.Add("barbara");
             // employees.Add("billy");
 
             //collect user vales until the value is an empty string
             while (true)
             {
-                Console.WriteLine("Please enter a name (leave empty to exit):");
+                Console.WriteLine("Please enter employee's first name (leave empty to exit):");
                 //get a name from the console and assign it to a variable
                 //?? is a "null coalescing operator" - similar to ternary operator - will check for null and replace it with the value after the operator. Or... it's supposed to do that...
-                string input = Console.ReadLine() ?? "";
+                string firstName = Console.ReadLine() ?? "";
                 //break if the user hits ENTER without typing a name
-                if (input == "")
+                if (firstName == "")
                 {
                     break;
                 }
-                employees.Add(input);
+                
+                Console.Write("Please enter employee's last name: ");
+                string lastName = Console.ReadLine() ?? "";
+
+                Console.Write("Please Enter Employee ID: ");
+                    //Parse to turn ReadLine input into an int
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+
+                Console.Write("Please enter Photo URL: ");
+                string photoUrl = Console.ReadLine() ?? "";
+
+                //new employee instance
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                
+                employees.Add(currentEmployee);
             }
                 return employees;
         }
 
-        static void PrintEmployees(List<string> employees) 
+        static void PrintEmployees(List<Employee> employees) 
         {
              Console.WriteLine("The employees are as follows:");
             for (int i = 0; i < employees.Count; i++)
                 {
-                    Console.WriteLine(employees[i]);
-                }
+                    string template ="{0,-10}\t{1,-20}\t{2}";
+                      // each item in employees is now an Employee instance
+                      // string format with template formatting check
+                      // basically, offers a template for how to space everything out, then it places each argument in, based on the padding
+                      Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+	                }
             
         }
 
         static void Main(string[] args)
         {
-            List<string> employees = GetEmployees();
+            List<Employee> employees = GetEmployees();
             PrintEmployees(employees);
         }
     }
